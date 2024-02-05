cde5df2b0865eb5ea25ea7393ddf1d7aa5beb097
amo95 <jameaamo@gmail.com>
2024-02-05 20:20:05 +0000

    first commit

---
+++ src/main/java/com/service/rabbitmqarchitectureintegration/RabbitmqArchitectureIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.service.rabbitmqarchitectureintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqArchitectureIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqArchitectureIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/rabbitmqarchitectureintegration/RabbitmqArchitectureIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.rabbitmqarchitectureintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqArchitectureIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

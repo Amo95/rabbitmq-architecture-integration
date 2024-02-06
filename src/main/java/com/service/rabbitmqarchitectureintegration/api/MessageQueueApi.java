917ad292088e193c1fd8db3165e054725764a2e1
amo95 <jameaamo@gmail.com>
2024-02-06 23:15:00 +0000

    created message sender api

---
+++ src/main/java/com/service/rabbitmqarchitectureintegration/api/MessageQueueApi.java
@@ -0,0 +1,23 @@
+package com.service.rabbitmqarchitectureintegration.api;
+
+import com.service.rabbitmqarchitectureintegration.publisher.MqProducer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/mq")
+public class MessageQueueApi {
+
+    private final MqProducer mqProducer;
+
+    public MessageQueueApi(MqProducer mqProducer) {
+        this.mqProducer = mqProducer;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<Void> sendMessage(@RequestParam("message") String message){
+        mqProducer.sendMessage(message);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
